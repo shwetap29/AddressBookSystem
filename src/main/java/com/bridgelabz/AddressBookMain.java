77fc2d00c6cc33c02368007b75ec3f0d81104b46
shwetap29 <shwetaprasad48@gmail.com>
2021-06-21 08:24:59 +0530

    ADDED:Created Contacts In adreess book

--- src/main/java/com/bridgelabz/AddressBookMain.java
+++ src/main/java/com/bridgelabz/AddressBookMain.java
@@ -3,6 +3,18 @@ package com.bridgelabz;
 public class AddressBookMain {
     public static void main(String[] args){
         System.out.println("Welcome To Address Book System");
+        ContactDetails person = new ContactDetails();
+
+        person.setFirstName("Shweta");
+        person.setLastName("Prasad");
+        person.setAddress("Hinjewadi Phase1");
+        person.setCity("Pune");
+        person.setState("Maharashtra");
+        person.setZip(411057);
+        person.setPhoneNumber("7894561230");
+        person.setEmail("shwetaprasad48@gmail.com");
+
+        System.out.println(person);
     }
 }
 
---
+++ src/main/java/com/bridgelabz/ContactDetails.java
@@ -0,0 +1,96 @@
+package com.bridgelabz;
+
+public class ContactDetails {
+
+    //declaring contact details variables
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String city;
+    private String state;
+    private int zip;
+    private String phoneNumber;
+    private String email;
+
+    // access private fields using getters and setters
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public int getZip() {
+        return zip;
+    }
+
+    public void setZip(int zip) {
+        this.zip = zip;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String s) {
+        this.email =email;
+    }
+
+    //Overridden Java toString method
+
+    @Override
+    public String toString() {
+        return "ContactDetails{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", address='" + address + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", zip=" + zip +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
